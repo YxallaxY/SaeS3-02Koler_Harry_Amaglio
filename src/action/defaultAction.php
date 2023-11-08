e0efe7850c5e1329c0e8ef354e092b6b7c184172
alexxela57 <alexandre.hary3@etu.univ-lorraine.fr>
2023-11-08 14:56:21 +0100

    ajout css + modif dans action

--- src/action/defaultAction.php
+++ src/action/defaultAction.php
@@ -17,12 +17,13 @@ class defaultAction
         ////$pdo->query("INSERT INTO touite (idUtil,tailleT,datePubli,chemin,note,contenue) VALUES (1,36,07/11/23,'blabla',36,'Touite de test #SALUT #SAE');");
 
         $pdo = \touiteur\bd\ConnectionFactory::makeConnection();
-        $s = "<h2>Affiche les 10 touite avec le plus de like</h2></br>";
+        $s="";
 
         $query = $pdo->query('SELECT * FROM `touite` ORDER BY note desc limit 10');
         $s = $s . "<div class='container'>";
+        $s = $s . "<h2>Affiche les 10 touite avec le plus de like</h2></br>";
         while ($data = $query->fetch()) {
-            $s = $s . "<div class='content_element'><br>" . $data['idTouite'] . " " . $data['idUtil'] . "</br>" . $data['contenueTouite'] . "</br>" . "Likes : " . $data['note'] . " " . "date :" . $data['datePubli'] . "</br></div>";
+            $s = $s . "<div class='content_element'>" . $data['idTouite'] . " " . $data['idUtil'] . "</br>" . $data['contenuTouite'] . "</br>" . "Likes : " . $data['note'] . " " . "date :" . $data['datePubli'] . "</br></div>";
         }
         $s = $s . "</div>";
         return $s;
